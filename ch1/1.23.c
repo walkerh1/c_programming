b46c221450432926749f57865eebc89e673f4505
Hugo Walker-Smith <hugowalkersmith@gmail.com>
2023-12-13 12:03:17 +1100

    exercise 1.23

---
+++ ch1/1.23.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+
+int main(void) {
+    int c, prev;
+
+    while ((c = getchar()) != EOF) {
+        if (c == '"') {
+            // print everything inside double quotes
+            putchar(c);
+            while ((c = getchar()) != EOF && c != '"') {
+                putchar(c);
+            }
+            putchar(c);
+        } else if (c == '\'') {
+            // print everything inside single quotes
+            putchar(c);
+            while((c = getchar()) != EOF && c != '\'') {
+                putchar(c);
+            }
+            putchar(c);
+        } else if (c == '/') {
+            prev = c;
+            c = getchar();
+            if (c == '*') {
+                // found '/*' - skip over everything until '*/' is reached
+                while ((c = getchar()) != EOF) {
+                    if (c == '*') {
+                        if ((c = getchar()) == '/') {
+                            break;
+                        }
+                    }
+                }
+            } else if (c == '/') {
+                // found '//' - skip over the rest of line
+                while ((c = getchar()) != EOF && c != '\n');
+                if (c == '\n') {
+                    putchar(c);
+                }
+            } else {
+                // found '/' in a non-comment context
+                putchar(prev);
+                putchar(c);
+            }
+        } else {
+            putchar(c);
+        }
+    }
+}
