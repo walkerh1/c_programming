12b77b7ff64390e1e5db10449f1c7c279565bcb3
Hugo Walker-Smith <hugowalkersmith@gmail.com>
2023-12-13 14:35:20 +1100

    exercise 1.22 update

--- ch1/1.22.c
+++ ch1/1.22.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
-#define MAXCOLUMN 10
-#define TABWIDTH 8
-#define WORDWRAP 12
+#define MAXCOLUMN 20    // max number of columns per line
+#define TABWIDTH 8      // number of columns in a tab block
+#define WORDWRAP 8      // if most recent blank is more than WORDWRAP from end of line, split word with '-'
 
 int update_line(char line[], int pos);
 int find_last_blank(const char line[], int n);
@@ -21,7 +21,19 @@ int main(void) {
             continue;
         }
 
-        line[pos] = c;
+        if (c == '\t') {
+            // replace tabs with appropriate number of spaces
+            int spaces = TABWIDTH - (pos % TABWIDTH);
+            while (spaces > 0 && pos < MAXCOLUMN) {
+                line[pos] = ' ';
+                pos++;
+                spaces--;
+            }
+            pos--;
+        } else {
+            line[pos] = (char) c;
+        }
+
         if (pos == MAXCOLUMN) {
             pos = find_last_blank(line, pos);
             if (pos == -1) {
@@ -41,6 +53,7 @@ int main(void) {
     print_up_to(line, pos);
 }
 
+// put wrapped portion of line at beginning of line buffer
 int update_line(char line[], int pos) {
     int i, j;
     for (i = 0, j = pos; j <= MAXCOLUMN; i++, j++) {
@@ -49,13 +62,18 @@ int update_line(char line[], int pos) {
     return i-1;
 }
 
+// find last blank in line buffer, or WORDWRAP limit if that is reached first
 int find_last_blank(const char line[], int n) {
-    while (n >= 0 && line[n] != ' ') {
+    while (n >= MAXCOLUMN - WORDWRAP && line[n] != ' ') {
         n--;
     }
-    return n==-1 ? -1 : n+1;
+    if (n < MAXCOLUMN - WORDWRAP) {
+        return -1;
+    }
+    return n+1;
 }
 
+// print line up to but not including pos, trimming leading whitespace
 void print_up_to(char line[], int pos) {
     int in_leading_whitespace = 1;
     for (int i = 0; i < pos; i++) {
