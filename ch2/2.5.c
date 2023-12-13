9e4b068c9ef8e13c27de32335e25f27391133029
Hugo Walker-Smith <hugowalkersmith@gmail.com>
2023-12-14 01:56:46 +1100

    exercise 2.5

---
+++ ch2/2.5.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int any(const char s1[], const char s2[]);
+
+int main(void) {
+    char s1[] = "abcdefg";
+    char s2[] = "gb";
+
+    printf("%d\n", any(s1, s2));
+}
+
+int any(const char s1[], const char s2[]) {
+    int i, j;
+    for (i = 0; s1[i] != '\0'; i++) {
+        for (j = 0; s2[j] != '\0'; j++) {
+            if (s1[i] == s2[j]) {
+                return i;
+            }
+        }
+    }
+    return -1;
+}
