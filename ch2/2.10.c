31e997da83dbac46743ec4c406f52d0ecae2cb27
Hugo Walker-Smith <hugowalkersmith@gmail.com>
2023-12-14 17:31:23 +1100

    exercise 2.10

---
+++ ch2/2.10.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int lower(int c);
+
+int main(void) {
+    putchar(lower('a'));
+    putchar(lower('A'));
+    putchar(lower('c'));
+    putchar(lower('C'));
+    putchar(lower('z'));
+    putchar(lower('Z'));
+    putchar(lower('?'));
+    putchar(lower('3'));
+}
+
+int lower(int c) {
+    return (c >= 'A' && c <= 'Z') ? c + 'a' - 'A' : c;
+}
