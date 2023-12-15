53dd5481b3f1864f04c1c50a858d2baaa9d4db64
Hugo Walker-Smith <hugowalkersmith@gmail.com>
2023-12-15 12:02:57 +1100

    exercise 3.5

---
+++ ch3/3.5.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <string.h>
+
+#define MAXLEN 10 // max number of decimal digits in signed/unsigned int is 9, +1 for possible '-' sign
+
+void itob(int n, char s[], int base);
+void reverse(char s[]);
+unsigned abs(int n);
+
+int main(void) {
+    char num[MAXLEN];
+    itob(233, num, 2); printf("itob(%d, %d)  = %s\n", 233, 2, num);
+    itob(233, num, 3); printf("itob(%d, %d)  = %s\n", 233, 3, num);
+    itob(233, num, 4); printf("itob(%d, %d)  = %s\n", 233, 4, num);
+    itob(233, num, 5); printf("itob(%d, %d)  = %s\n", 233, 5, num);
+    itob(233, num, 6); printf("itob(%d, %d)  = %s\n", 233, 6, num);
+    itob(233, num, 7); printf("itob(%d, %d)  = %s\n", 233, 7, num);
+    itob(233, num, 8); printf("itob(%d, %d)  = %s\n", 233, 8, num);
+    itob(233, num, 9); printf("itob(%d, %d)  = %s\n", 233, 9, num);
+    itob(233, num, 10); printf("itob(%d, %d) = %s\n", 233, 10, num);
+    itob(233, num, 11); printf("itob(%d, %d) = %s\n", 233, 11, num);
+    itob(233, num, 12); printf("itob(%d, %d) = %s\n", 233, 12, num);
+    itob(233, num, 13); printf("itob(%d, %d) = %s\n", 233, 13, num);
+    itob(233, num, 14); printf("itob(%d, %d) = %s\n", 233, 14, num);
+    itob(233, num, 15); printf("itob(%d, %d) = %s\n", 233, 15, num);
+    itob(233, num, 16); printf("itob(%d, %d) = %s\n", 233, 16, num);
+
+    return 0;
+}
+
+void itob(int n, char s[], int base) {
+    int i, rem, sign;
+
+    if ((sign = n) < 0) {
+        n = -n;
+    }
+
+    i = 0;
+    do {
+        rem = abs(n % base);
+        s[i++] = rem < 10 ? rem + '0' : rem - 10 + 'A';
+    } while ((n /= base) != 0);
+
+    if (sign < 0) {
+        s[i++] = '-';
+    }
+
+    s[i] = '\0';
+
+    reverse(s);
+}
+
+void reverse(char s[]) {
+    int i, j;
+    char tmp;
+    for (i = 0, j = strlen(s) - 1; i < j; i++, j--) {
+        tmp = s[i];
+        s[i] = s[j];
+        s[j] = tmp;
+    }
+}
+
+unsigned abs(int n) {
+    return n < 0 ? -n : n;
+}
