fec34201a23583069695cdccfcce3966622f4217
Hugo Walker-Smith <hugowalkersmith@gmail.com>
2023-12-19 19:57:28 +1100

    exercise 4.4

--- ch4/4.3.to.10.c
+++ ch4/4.3.to.10.c
@@ -4,6 +4,10 @@
 
 #define MAXOP 100   // max size of value or operator
 #define NUMBER '0'  // signal that a number was found
+#define PEEK '1'    // signal to execute peek command: write 'pk' in calculator
+#define DUP '2'     // signal to execute duplicate command: write 'du' in calculator
+#define SWAP '3'    // signal to execute swap command: write 'sw' in calculator
+#define CLEAR '4'   // signal to execute clear command: write 'cl' in calculator
 #define MAXVAL 100  // max depth of stack
 #define BUFSIZE 100 // max size of buffer for ungetch
 
@@ -21,13 +25,40 @@ int bufp;           // next free position in buf
 // reverse polish calculator
 int main(void) {
     int type;
-    double op2;
+    double op1, op2;
     char s[MAXOP];
 
     while ((type = getop(s)) != EOF) {
         switch (type) {
             case NUMBER:
                 push(atof(s));
+                break;
+            case PEEK:
+                if (sp > 0) {
+                    printf("\t%.8g\n", val[sp-1]);
+                } else {
+                    printf("error: stack is empty, nothing to peek at\n");
+                }
+                break;
+            case DUP:
+                if (sp > 0) {
+                    push(val[sp-1]);
+                } else {
+                    printf("error: stack is empty, nothing to duplicate\n");
+                }
+                break;
+            case SWAP:
+                if (sp > 1) {
+                    op1 = pop();
+                    op2 = pop();
+                    push(op1);
+                    push(op2);
+                } else {
+                    printf("error: can't swap, stack needs at least two elements\n");
+                }
+                break;
+            case CLEAR:
+                sp = 0;
                 break;
             case '+':
                 push(pop() + pop());
@@ -46,7 +77,7 @@ int main(void) {
                     printf("error: zero division\n");
                 }
                 break;
-            case '%':   // exercise 4.3
+            case '%':
                 if ((op2 = pop()) != 0.0) {
                     push((int)pop() % (int)op2);
                 } else {
@@ -92,6 +123,42 @@ int getop(char s[]) {
         ;
 
     s[1] = '\0';
+
+    // check for pk command
+    if (c == 'p') {
+        if ((c = getch()) == 'k') {
+            return PEEK;
+        } else {
+            return 'p';
+        }
+    }
+
+    // check for du command
+    if (c == 'd') {
+        if ((c = getch()) == 'u') {
+            return DUP;
+        } else {
+            return 'd';
+        }
+    }
+
+    // check for sw command
+    if (c == 's') {
+        if ((c = getch()) == 'w') {
+            return SWAP;
+        } else {
+            return 's';
+        }
+    }
+
+    // check for cl command
+    if (c == 'c') {
+        if ((c = getch()) == 'l') {
+            return CLEAR;
+        } else {
+            return 'c';
+        }
+    }
 
     // not a number
     if (!isdigit(c) && c != '.' && c != '-') {
